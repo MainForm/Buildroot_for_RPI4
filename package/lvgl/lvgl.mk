83b7a6d2eadfb5a3c30b06f87e2bddbf76537b77
Gordon <bkgr4949p@icloud.com>
2025-08-08 09:43:05 +0900

    Add the lvgl configure to enable DRM

--- package/lvgl/lvgl.mk
+++ package/lvgl/lvgl.mk
@@ -4,12 +4,18 @@
  #
  ################################################################################
 
-LVGL_VERSION = v0.0.3
+LVGL_VERSION = v0.0.4
 LVGL_SITE = https://github.com/MainForm/buildroot_lvgl_package
 LVGL_SITE_METHOD = git
 LVGL_GIT_SUBMODULES = YES
 
 LVGL_INSTALL_STAGING = YES
 LVGL_INSTALL_TARGET = NO
+
+ifeq ($(BR2_PACKAGE_LVGL_USE_DRM),y)
+LVGL_CONF_OPTS += -DLV_USE_LINUX_DRM=1
+else
+LVGL_CONF_OPTS += -DLV_USE_LINUX_DRM=0
+endif
 
 $(eval $(cmake-package))
